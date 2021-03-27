15278bc3103cd0367f1f4e2c118201e9303d2472
André Tavares Machado <andre266@outlook.com.br>
2021-03-27 18:51:59 -0300

    Atualização do projeto

--- index.php
+++ index.php
@@ -80,6 +80,15 @@ $app->get('/admin/users/create', function(){
 $app->get("/admin/users/:iduser/delete", function($iduser){
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
 	
 });
 
@@ -87,9 +96,15 @@ $app->get("/admin/users/:iduser", function($iduser){
 	
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
 	$page = new PageAdimin();
 
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"=>$user->getValues()
+	));
 });
 
 $app->post("/admin/users/create", function(){
@@ -112,9 +127,36 @@ $app->post("/admin/users/create", function(){
 $app->post("/admin/users/:iduser", function($iduser){
 
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 	
 });
 
+$app->get("/admin/forgot", function(){
+	
+	$page = new PageAdimin([
+		"header" => false,
+		"footer" => false
+	]);
+
+	$page->setTpl("forgot");
+});
+
+$app->post("/admin/forgot", function(){
+	
+	$user = User::getForgot($_POST["email"]);
+});
 
 $app->run();
 
